73f029ca89fd980db86949fe8d0dcd33d5d7bd7f
Tobias Slade-Harajda <tobias.slade-harajda@warwick.ac.uk>
2023-09-19 14:49:25 +0100

    small changes to test codes

--- empty_run.py
+++ empty_run.py
@@ -6,44 +6,7 @@ sim_lst = ['lowres_D_He3/0_25_p_90']
 quant = 'Magnetic_Field_Bz'
 
 for sim in sim_lst:
-	loc = getSimulation('/storage/space2/phrmsf/ECRH/ECRH_JT60U_5')#'+sim)/home/space/phrmsf/Documents/EPOCH/epoch-4.17.16/epoch1d/old/0006qp
+	loc = getSimulation('/storage/space2/phrmsf/lowres_D_He3/0_34_p_90')
 	d0 = sdfread(0)
 	times = read_pkl('times')
-	wce = getCyclotronFreq(d0,'Electrons')
-	wcp = getCyclotronFreq(d0,'Protons')
-	tce = 2*const.PI/wce
-	tcp = 2*const.PI/wcp
-	dt = (times[-1]-times[0])/len(times)
-	print(dt,dt/tce,dt/tcp)
-	print(times[-1],times[-1]/tce,times[-1]/tcp)
-	sys.exit()
-
-	omegas = wcD*np.linspace(0,25,1000)
-	k1, k2, k3 = coldplasmadispersion(d0, 'Deuterons', 'Tritons', omegas, theta=89)
-
-
-	L = getGridlen(d0)
-	FT2d = read_pkl('FT_2d_'+quant)
-	vA = getAlfvenVel(d0)
-	times = read_pkl('times')
-	dx = getdxyz(d0)
-	dt = getdt(times)
-	wcp = getCyclotronFreq(d0,'Deuterons')
-	tcp = 2*const.PI/wcp
-	wlim = 0.5*2*const.PI/dt
-	klim = 0.5*2*const.PI/dx
-	(nw,nk) = FT2d.shape
-	wmax = wlim#25*wcp
-	kmax = klim#50*wcp/vA
-	dw = 0.5*2*const.PI/times[-1]
-	dk = 0.5*2*const.PI/L
-	print(dw/getCyclotronFreq(d0,'Deuterons'))
-#	FT2d = FT2d[:int(nw*wmax/wlim),:int(nk*kmax/klim)]
-	# plot
-	fig, ax = plt.subplots(figsize=(8,4)) #(8,4)
-	ax.imshow(np.log10(FT2d),**kwargs,extent=[0,kmax*vA/wcp,0,wmax/wcp],cmap='magma',clim=(-4,6))
-	ax.set_xlabel(getWavenumberLabel('Deuterons'),**tnrfont)
-	ax.set_ylabel(r'$\omega/$'+getOmegaLabel('Deuterons'),**tnrfont)
-	plt.show()
-	#fig.savefig('FT_2d_'+quant+'.png',bbox_inches='tight')
-
+	
---
+++ lib/ASCII_logo.py
@@ -0,0 +1,7 @@
+import pyfiglet
+
+print(pyfiglet.figlet_format('Batch load',font='puffy',justify='center',width=110))
+print(pyfiglet.figlet_format('made by T. W. S - H',font='mini',justify='center',width=110))
+print(pyfiglet.figlet_format('CFSA - University of Warwick',font='mini',justify='center',width=110))
+
+del pyfiglet
