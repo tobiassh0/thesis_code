4688e40cfa7b4c33d545ed5ae84d3f3092108d0e
Tobias Slade-Harajda <tobias.slade-harajda@warwick.ac.uk>
2023-11-10 09:53:03 +0000

    tidy lowres analysis

--- lowres_analysis.py
+++ lowres_analysis.py
@@ -1,6 +1,4 @@
 
-from func_load import *
-
 def func_exp(x,a,x0,sigma,offset):
 	return a*np.exp(-(x-x0)**2/(2*sigma**2))+offset
 
@@ -11,158 +9,134 @@ def func_linear(p,x):
 def func_quad(p,x):
 	return p[0]*x**2 + p[1]*x + p[2]
 
-#def func_exp(p,x):
-#	a,x0,sigma,offset = p
-#	return a*np.exp(-(x-x0)**2/(2*sigma**2))+offset
-
-#def getCurveFit(x,y,xerr,yerr,func='linear',beta0=[1.0,0.0],plot=True):
-#	""" if function isn't finding solutions then (1) try changing beta to closer values 
-#	and (2) change xerr and yerr to np.std(x), np.std(y), especially if they have zero 
-#	error.
-#	beta0 must be same length as functions needed to fit
-#	"""
-#	if func == 'linear':
-#		dfunc = func_linear
-#	elif func == 'quad':
-#		dfunc = func_quad
-#	else:
-#		print('## ERROR ## :: No valid function')
-#		sys.exit()
-#	from scipy.odr import *
-#	# setup model
-#	model = Model(dfunc)
-#	data = RealData(x=x,y=y,sx=xerr,sy=yerr)
-#	# make ODR
-#	myodr = ODR(data, model, beta0=beta0)
-#	myout = myodr.run()
-#	myout.pprint()
-#	if plot:
-#		x_fit = np.linspace(x[0],x[-1],100)
-#		y_fit = dfunc(myout.beta,x_fit)
-#		plt.plot(x_fit,y_fit,color='r',linestyle='--')
-#		plt.savefig('{}_fit.png'.format(func))
-#	return None
-
-os.chdir('/storage/space2/phrmsf/lowres_D_He3/')
-sims = np.sort([i for i in os.listdir() if 'p_90' in i])
-xiHe3 = np.array([int(i[2:4])/100 for i in sims])
-home = os.getcwd()
-
-# compare power spectra trend
-	# shared area offset, relevant to 5% concentration
-		# control
-wcp = 3.7*const.qe/getMass('Protons') # Bq/m
-simloc = getSimulation(sims[0])
-power5 = 10**read_pkl('log10_power')
-omegas5 = read_pkl('omegas_power')
-dw = omegas5[-1]/len(omegas5) 	
-power5/=dw # PSD
-thresh = omegas5/wcp < 20
-power5 = power5[thresh]
-omegas5= omegas5[thresh]
-#plt.plot(omegas5/wcp,np.log10(power5),label=hlabel[0])
-os.chdir(home)
-peaks=[0]
-colors=['k']
-perrs=[0]
-
-for i in range(1,len(sims)):
-	print(sims[i])
-	simloc = getSimulation(sims[i])
-	power = 10**read_pkl('log10_power')
-	omegas = read_pkl('omegas_power')
-	dw = omegas[-1]/len(omegas) 	
-	power/=dw # PSD
-	thresh = omegas/wcp < 20
-	power = power[thresh]
-	omegas= omegas[thresh]
-	w = np.linspace(-omegas[-1]/2,omegas[-1]/2,len(omegas))/wcp
-	# get cross correlation
-	crosscor = getCrossCorrelation(np.log10(power),np.log10(power5)) # allows for correct difference between curves
-	# weighted arithmetic mean
-	mean = np.sum(w*crosscor)/np.sum(crosscor) ; sigma = 5#np.sqrt(np.sum(crosscor*(w-mean)**2)/np.sum(crosscor))
-	offset = -10
-	# fit an exp curve (normalised to wcp)
-	popt, pcov = curve_fit(func_exp,w,crosscor,p0=[max(crosscor),mean,sigma,offset],maxfev=10000) # exponential fitting
-	print(*popt)
-	# append fit parameters and errors
-	peaks.append(popt[1])# a, x0, sigma, offset
-	perr = np.sqrt(np.diag(pcov))# one std on parameters
-	perrs.append(perr[1])
-	colors.append('b')
-#	plt.plot(w,crosscor)
-#	plt.plot(w,func_exp(w,*popt))
+def pearsonscc(sims,xiHe3):
+	home = os.getcwd()
+	# compare power spectra trend
+		# shared area offset, relevant to 5% concentration
+			# control
+	wcp = 3.7*const.qe/getMass('Protons') # Bq/m
+	simloc = getSimulation(sims[0])
+	power5 = 10**read_pkl('log10_power')
+	omegas5 = read_pkl('omegas_power')
+	dw = omegas5[-1]/len(omegas5) 	
+	power5/=dw # PSD
+	thresh = omegas5/wcp < 20
+	power5 = power5[thresh]
+	omegas5= omegas5[thresh]
+	#plt.plot(omegas5/wcp,np.log10(power5),label=hlabel[0])
 	os.chdir(home)
-
-fig,ax=plt.subplots(figsize=(7,5))
-for c,_x,_y,_yerr in zip(colors,xiHe3,peaks,perrs):
-	if _x == xiHe3[0]:
-		_yerr = None
-	ax.errorbar(_x,_y,yerr=_yerr,xerr=None,fmt='o',color=c,capsize=15)
-r = np.corrcoef(xiHe3,peaks)[0,1]
-ax.set_ylabel(r'$\omega_{off}/\Omega_p$',**tnrfont)
-ax.set_xlabel(r'$\xi_{He3}$',**tnrfont)
-ax.annotate(r'$r={}$'.format(np.around(r,2)),xy=(0.03,0.9),xycoords='axes fraction',ha='left',va='bottom',fontsize=18)
-ax.set_xlim(0.,0.5) ; ax.set_ylim(-0.05,0.45)
-xiHe3_err = np.std(xiHe3)
-perrs[0] = np.std(peaks) # re-write error on 5% xiHe3
-# fit ODR line of best fit with errors
-from scipy.odr import *
-linear_model = Model(func_linear)
-data = RealData(x=xiHe3,y=peaks,sx=xiHe3_err,sy=perrs)
-myodr = ODR(data, linear_model, beta0=[1,-0.1])
-myout = myodr.run()
-myout.pprint()
-x_fit = np.linspace(0,.5,100)
-y_fit = func_linear(myout.beta,x_fit)
-ax.plot(x_fit,y_fit,color='r',linestyle='--')
-ax.annotate(r'$d \omega_{off}/d \xi_{He3}=$'+r'$({}\pm{})$'.format(np.around(myout.beta[0],2),np.around(myout.sd_beta[0],2))+r'$\Omega_p$',\
-				xy=(0.95,0.05),xycoords='axes fraction',ha='right',va='bottom',fontsize=18)
-fig.savefig('PearsonsCorrCoef.png',bbox_inches='tight')
-plt.show()
-#np.savetxt('pearsonscrosscor.txt',np.array([xiHe3,peaks,perrs]).T)
-sys.exit()
-
-# cross correlation between number density and energy of protons
-for i in range(len(sims)):
-	print(sims[i])
-	simloc = getSimulation(sims[i])
-	ind_lst = list_sdf(simloc)
+	peaks=[0]
+	colors=['k']
+	perrs=[0]
 	
-	# quantities and ion species
-	name = 'density_energy'
-	species = getIonSpecies(sdfread(0))
-	quantities = ['Derived_Number_Density_'+i for i in species]
-	masses = [getMass(i) for i in species]
+	for i in range(1,len(sims)):
+		print(sims[i])
+		simloc = getSimulation(sims[i])
+		power = 10**read_pkl('log10_power')
+		omegas = read_pkl('omegas_power')
+		dw = omegas[-1]/len(omegas) 	
+		power/=dw # PSD
+		thresh = omegas/wcp < 20
+		power = power[thresh]
+		omegas= omegas[thresh]
+		w = np.linspace(-omegas[-1]/2,omegas[-1]/2,len(omegas))/wcp
+		# get cross correlation
+		crosscor = getCrossCorrelation(np.log10(power),np.log10(power5)) # allows for correct difference between curves
+		# weighted arithmetic mean
+		mean = np.sum(w*crosscor)/np.sum(crosscor) ; sigma = 5#np.sqrt(np.sum(crosscor*(w-mean)**2)/np.sum(crosscor))
+		offset = -10
+		# fit an exp curve (normalised to wcp)
+		popt, pcov = curve_fit(func_exp,w,crosscor,p0=[max(crosscor),mean,sigma,offset],maxfev=10000) # exponential fitting
+		print(*popt)
+		# append fit parameters and errors
+		peaks.append(popt[1])# a, x0, sigma, offset
+		perr = np.sqrt(np.diag(pcov))# one std on parameters
+		perrs.append(perr[1])
+		colors.append('b')
+	#	plt.plot(w,crosscor)
+	#	plt.plot(w,func_exp(w,*popt))
+		os.chdir(home)
 	
-	try: # load cross cor
-		raise SystemError
-		crosscorr = read_pkl(name)	
-	except: # calculate cross cor
-		# get number density and energy
-		narr = []
-		try:
-			for j in range(len(species)):
-				narr.append(load_batch_fieldmatrix([],quantities[j],para=False))
-		except:
-			get_batch_fieldmatrix(ind_lst,quantities=quantities,load=False)
-			for j in range(len(species)):
-				narr.append(load_batch_fieldmatrix([],quantities[j],para=False))
+	fig,ax=plt.subplots(figsize=(7,5))
+	for c,_x,_y,_yerr in zip(colors,xiHe3,peaks,perrs):
+		if _x == xiHe3[0]:
+			_yerr = None
+		ax.errorbar(_x,_y,yerr=_yerr,xerr=None,fmt='o',color=c,capsize=15)
+	r = np.corrcoef(xiHe3,peaks)[0,1]
+	ax.set_ylabel(r'$\omega_{off}/\Omega_p$',**tnrfont)
+	ax.set_xlabel(r'$\xi_{He3}$',**tnrfont)
+	ax.annotate(r'$r={}$'.format(np.around(r,2)),xy=(0.03,0.9),xycoords='axes fraction',ha='left',va='bottom',fontsize=18)
+	ax.set_xlim(0.,0.5) ; ax.set_ylim(-0.05,0.45)
+	xiHe3_err = np.std(xiHe3)
+	perrs[0] = np.std(peaks) # re-write error on 5% xiHe3
+	# fit ODR line of best fit with errors
+	linear_model = Model(func_linear)
+	data = RealData(x=xiHe3,y=peaks,sx=xiHe3_err,sy=perrs)
+	myodr = ODR(data, linear_model, beta0=[1,-0.1])
+	myout = myodr.run()
+	myout.pprint()
+	x_fit = np.linspace(0,.5,100)
+	y_fit = func_linear(myout.beta,x_fit)
+	ax.plot(x_fit,y_fit,color='r',linestyle='--')
+	ax.annotate(r'$d \omega_{off}/d \xi_{He3}=$'+r'$({}\pm{})$'.format(np.around(myout.beta[0],2),np.around(myout.sd_beta[0],2))+r'$\Omega_p$',\
+					xy=(0.95,0.05),xycoords='axes fraction',ha='right',va='bottom',fontsize=18)
+	fig.savefig('PearsonsCorrCoef.png',bbox_inches='tight')
+	plt.show()
+	#np.savetxt('pearsonscrosscor.txt',np.array([xiHe3,peaks,perrs]).T)
+
+def nEcc(sims,xiHe3):
+	# cross correlation between number density and energy of protons
+	for i in range(len(sims)):
+		print(sims[i])
+		simloc = getSimulation(sims[i])
+		ind_lst = list_sdf(simloc)
 		
-		# weighted number density according to mass
-		ntot = np.mean([masses[i]*narr[i] for i in range(len(species))],axis=0) # mean across each position and time
-		plt.imshow(ntot-np.mean(ntot[0:10,:]),**kwargs,cmap='jet')
-		plt.xlabel('x',**tnrfont) ; plt.xlabel('t',**tnrfont)
-		plt.colorbar()
-		plt.savefig(home+'/NumberDensity_{}.png'.format(xiHe3[i]))
+		# quantities and ion species
+		name = 'density_energy'
+		species = getIonSpecies(sdfread(0))
+		quantities = ['Derived_Number_Density_'+i for i in species]
+		masses = [getMass(i) for i in species]
+		
+		try: # load cross cor
+			raise SystemError
+			crosscorr = read_pkl(name)	
+		except: # calculate cross cor
+			# get number density and energy
+			narr = []
+			try:
+				for j in range(len(species)):
+					narr.append(load_batch_fieldmatrix([],quantities[j],para=False))
+			except:
+				get_batch_fieldmatrix(ind_lst,quantities=quantities,load=False)
+				for j in range(len(species)):
+					narr.append(load_batch_fieldmatrix([],quantities[j],para=False))
+			
+			# weighted number density according to mass
+			ntot = np.mean([masses[i]*narr[i] for i in range(len(species))],axis=0) # mean across each position and time
+			plt.imshow(ntot-np.mean(ntot[0:10,:]),**kwargs,cmap='jet')
+			plt.xlabel('x',**tnrfont) ; plt.xlabel('t',**tnrfont)
+			plt.colorbar()
+			plt.savefig(home+'/NumberDensity_{}.png'.format(xiHe3[i]))
+			plt.clf()
+			Ep = read_pkl('Protons_KEdensmatrix')
+			dEp = Ep-np.mean(Ep[0:10,:])
+			dEp = dEp/np.max(np.abs(dEp))
+			# calculate cross correlation
+			crosscorr = getCrossCorrelationMat(ntot,dEp)
+		# plot cross correlation
+		fig,ax=plotCrossCorrelationMat(crosscorr,ylabel='t',xlabel='x')
+		os.chdir(home)
+		fig.savefig('CrossCorrelation_{}.png'.format(xiHe3[i]))
 		plt.clf()
-		Ep = read_pkl('Protons_KEdensmatrix')
-		dEp = Ep-np.mean(Ep[0:10,:])
-		dEp = dEp/np.max(np.abs(dEp))
-		# calculate cross correlation
-		crosscorr = getCrossCorrelationMat(ntot,dEp)
-	# plot cross correlation
-	fig,ax=plotCrossCorrelationMat(crosscorr,ylabel='t',xlabel='x')
-	os.chdir(home)
-	fig.savefig('CrossCorrelation_{}.png'.format(xiHe3[i]))
-	plt.clf()
+
+
+if __name__ == '__main__':
+	from func_load import *
+	from scipy.odr import *
+	os.chdir('/storage/space2/phrmsf/lowres_D_He3/')
+	sims = np.sort([i for i in os.listdir() if 'p_90' in i])
+	xiHe3 = np.array([int(i[2:4])/100 for i in sims])
+	#pearsonscc(sims,xiHe3)
+	#nEcc(sims,xiHe3)
+	
+	
--- test.py
+++ test.py
@@ -1,27 +1,78 @@
 
 from func_load import *
-import correlation.spatial_crosscor as sc
+#import correlation.spatial_crosscor as sc
 
+def t1():
+	wca = 2.1 * (2 * abs(const.qe))/getMass('Alphas')
+	w = np.arange(0,40,0.01) * wca
+	
+	simloc = getSimulation('/storage/space2/phrmsf/traceT/traceT_0_00')
+	#dphase,Rtdx = sc.getSpatialCorrelation(sdfread(0),plot=False)
+	vA = getAlfvenVel(sdfread(0))
+	
+	## cold plasma disp frequencies
+	_,k2,_= coldplasmadispersion(sdfread(0),w,theta=89.)
+	k2 *= (vA/wca)
+	plt.xlim(0,100)
+	plt.scatter(k2,w/wca,facecolor='b',edgecolor='b')
+	
+	## 1994 oblique (24)
+	theta = 89. * const.PI/180.
+	k = np.arange(0,1000,0.01) * (wca/vA)
+	kpara = k*np.cos(theta)
+	w2 = (k**2 + kpara**2 + (k*kpara)**2 * (vA/wca)**2 )**2 - (2*k*kpara)**2
+	tw2 = 0.5*(vA**2)*(k**2 + kpara**2 + (k*kpara)**2 * (vA/wca)**2 + np.sqrt(w2))
+	tw = np.sqrt(tw2)
+	plt.scatter(k*vA/wca,tw/wca,facecolor='r',edgecolor='r')
+	plt.show()
 
-wca = 2.1 * (2 * abs(const.qe))/getMass('Alphas')
-w = np.arange(0,40,0.01) * wca
+def t2():
+	sims = ['A_B_neq','A_B_C_neq','A_B_eq','A_B_C_eq']
+	fig,axs = plt.subplots(nrows=2,ncols=2,figsize=(15.5,9),sharex=True,sharey='row')
+	fig.subplots_adjust(hspace=0.07,wspace=0.07)
+	ax = axs.ravel()
+	i=0
+	for sim in sims:
+		simloc = getSimulation('/home/space/phrmsf/Documents/EPOCH/5_devel/epoch1d/'+sim)
+		times = read_pkl('times')
+		tcp = 2*const.PI/getCyclotronFreq(sdfread(0),'Protons')
+		species = getAllSpecies(sdfread(0))
+		print(sim,species)
+		if len(species)==3: # protons, protons, electrons
+			colors = ['k','r','orange']
+			labels = [r'$e$',r'$p$',r'$p$']
+			E0 = read_pkl(species[0]+'_KE') # elec
+			E1 = read_pkl(species[1]+'_KE') # p
+			E2 = read_pkl(species[2]+'_KE') # p
+			E3 = np.zeros(len(E0))
+			ax[i].plot(times/tcp,E0-np.mean(E0[0:10]),color=colors[0],label=labels[0])
+			ax[i].plot(times/tcp,E1-np.mean(E1[0:10]),color=colors[1],label=labels[1])
+			ax[i].plot(times/tcp,E2-np.mean(E2[0:10]),color=colors[2],label=labels[2])
+			ax[i].legend(loc='best',ncol=len(species),borderpad=0.0,labelspacing=0.05,prop={'size':18})
+			ax[i].tick_params(axis='both', which='major', labelsize=18)
+		elif len(species)==4: # deuterons, tritons, protons, electrons
+			colors = ['k','b','g','orange']
+			labels = [r'$e$',r'$D_2$',r'$T_3$',r'$p$']
+			E0 = read_pkl(species[0]+'_KE') # elec
+			E1 = read_pkl(species[1]+'_KE') # D
+			E2 = read_pkl(species[2]+'_KE') # T
+			E3 = read_pkl(species[3]+'_KE') # p
+			ax[i].plot(times/tcp,E0-np.mean(E0[0:10]),color=colors[0],label=labels[0])
+			ax[i].plot(times/tcp,E1-np.mean(E1[0:10]),color=colors[1],label=labels[1])
+			ax[i].plot(times/tcp,E2-np.mean(E2[0:10]),color=colors[2],label=labels[2])
+			ax[i].plot(times/tcp,E3-np.mean(E3[0:10]),color=colors[3],label=labels[3])
+			ax[i].legend(loc='best',ncol=len(species),borderpad=0.0,labelspacing=0.05,prop={'size':18})
+			ax[i].tick_params(axis='both', which='major', labelsize=18)
+		else: # n/a
+			raise SystemExit
+		i+=1
+	ax[0].set_xlim(0,1)
+	ax[0].set_ylabel(r'$\Delta u$'+'  ['+r'$J/m^3$'+']',**tnrfont)
+	ax[2].set_ylabel(r'$\Delta u$'+'  ['+r'$J/m^3$'+']',**tnrfont)
+	ax[2].set_xlabel(r'$t/\tau_{cp}$',**tnrfont)
+	ax[3].set_xlabel(r'$t/\tau_{cp}$',**tnrfont)
+	fig.savefig('/home/space/phrmsf/Documents/EPOCH/5_devel/epoch1d/NDW_collection.png',bbox_inches='tight')
 
-simloc = getSimulation('/storage/space2/phrmsf/traceT/traceT_0_00')
-#dphase,Rtdx = sc.getSpatialCorrelation(sdfread(0),plot=False)
-vA = getAlfvenVel(sdfread(0))
-
-## cold plasma disp frequencies
-_,k2,_= coldplasmadispersion(sdfread(0),w,theta=89.)
-k2 *= (vA/wca)
-plt.xlim(0,100)
-plt.scatter(k2,w/wca,facecolor='b',edgecolor='b')
-
-## 1994 oblique (24)
-theta = 89. * const.PI/180.
-k = np.arange(0,1000,0.01) * (wca/vA)
-kpara = k*np.cos(theta)
-w2 = (k**2 + kpara**2 + (k*kpara)**2 * (vA/wca)**2 )**2 - (2*k*kpara)**2
-tw2 = 0.5*(vA**2)*(k**2 + kpara**2 + (k*kpara)**2 * (vA/wca)**2 + np.sqrt(w2))
-tw = np.sqrt(tw2)
-plt.scatter(k*vA/wca,tw/wca,facecolor='r',edgecolor='r')
-plt.show()
+if __name__ == '__main__':
+	#t1()
+	t2()
