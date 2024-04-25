3afdc06470dbfae697bcfb8a7a512ff16b9f43b1
Tobias Slade-Harajda <tobias.slade-harajda@warwick.ac.uk>
2024-04-25 14:23:32 +0100

    test and push

--- energy/Ex_Bz_ratio.py
+++ energy/Ex_Bz_ratio.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 from func_load import *
 
 # get the ratio between Ex and Delta Bz field energy densities through time and plot each time trace in a panel 
-def Ex_Bz(sims,sim0=[],xarr,minspec='Protons',mean_to=10,home=None):
+def Ex_Bz(sims,xarr,sim0=[],minspec='Protons',mean_to=10,home=None):
     """
         In:
+            sims : list of sims to loop over and compare (must have len > 1)
             xarr : x array which is looped over, same len as sims (i.e. xiHe3)
             minspec : minority species to normalise time to
             mean_to : # time files to take mean to
@@ -57,12 +58,13 @@ def Ex_Bz(sims,sim0=[],xarr,minspec='Protons',mean_to=10,home=None):
 
     return fratio, rxarr
 
-def f1_f2(sims,sim0=[],xarr,minspec='Protons',mean_to=10,home=None):
+def f1_f2(sims,xarr,f1='Electric_Field_Ex',f2='Magnetic_Field_Bz',sim0=[],minspec='Protons',mean_to=10,home=None,ylabel=None):
     """
         In:
+            sims : list of sims to loop over and compare (must have len > 1)
+            xarr : x array which is looped over, same len as sims (i.e. xiHe3)
             f1 : name of first field
             f2 : name of second field
-            xarr : x array which is looped over, same len as sims (i.e. xiHe3)
             minspec : minority species to normalise time to
             mean_to : # time files to take mean to
             home : location to save fig, if None then save in cwd
@@ -70,12 +72,15 @@ def f1_f2(sims,sim0=[],xarr,minspec='Protons',mean_to=10,home=None):
             fratio : ratio between field energy densities f1/f2
             rxarr : matching coordinates for each ratio through time per xarr[i]
     """
-    f1name,f1label,f1mult = Endict(f1)[0]
-    f2name,f2label,f2mult = Endict(f2)[0] # name of energy field
+    f1name,f1label,f1mult = Endict(f1)
+    f2name,f2label,f2mult = Endict(f2) # name of energy field
     if not home:
         home = os.getcwd()
     if sim0 == []:
         sim0 = sims[0]
+    if ylabel == None:
+        ylabel = '('+f1label+')'+r'$/$'+'('+f2label+')'
+
     _=getSimulation(sim0)
     times = read_pkl('times')
     tcmin = 2*const.PI/getCyclotronFreq(sdfread(0),minspec)
@@ -108,7 +113,7 @@ def f1_f2(sims,sim0=[],xarr,minspec='Protons',mean_to=10,home=None):
     ax[0].locator_params(axis='x',nbins=6)
     ax[0].set_xlim(0,times[-1]/tcmin)
     fig.supxlabel(r'$t$'+getOmegaLabel(minspec)+r'$/2\pi$',**tnrfont)
-    fig.supylabel(f1label+r'$/$'+f2label,**tnrfont)
+    fig.supylabel(ylabel,**tnrfont)
     fig.savefig(home+f1name+'_'+f2name+'_time.png',bbox_inches='tight')
 
     return fratio, rxarr
@@ -127,6 +132,8 @@ if __name__=='__main__':
     sims = sims[1:]
     xiHe3 = [float(i.split('_')[1]) for i in sims]
 
-    fratio, rxiHe3 = Ex_Bz(sims,sim0=sim0,xarr=xiHe3)
+    # fratio, rxiHe3 = Ex_Bz(sims,xarr=xiHe3,sim0=sim0)
+    fratio, rxiHe3 = f1_f2(sims,xiHe3,'Electric_Field_Ex','Magnetic_Field_Bz',sim0=sim0,home=home,\
+                            ylabel=r'$E_x^2/\Delta B_z^2$'+'  '+r'$[\epsilon_0\mu_0]$')
     # plt.scatter(rxiHe3,fratio,alpha=0.1)
     # plt.show()
